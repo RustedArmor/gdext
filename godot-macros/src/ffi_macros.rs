32488081c9c162108b4fc5917dd5bcc4a406272a
Lukas Rieger <code@lukasrieger.com>
2025-06-15 22:03:25 +0200

    generate __godot_rust_registrant_ names based on crate name

--- godot-macros/src/ffi_macros.rs
+++ godot-macros/src/ffi_macros.rs
@@ -13,51 +13,29 @@ use crate::util::bail;
 use crate::ParseResult;
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::env;
 
+use std::sync::atomic::{AtomicU32, Ordering};
 
-static SRAND_INITIALIZED: AtomicBool = AtomicBool::new(false);
-
-// Create sufficiently unique identifier without entire `uuid` (let alone `rand`) crate dependency.
-struct TrivialRng;
-
-impl TrivialRng {
-    fn rand() -> u32 {
-        if SRAND_INITIALIZED.compare_exchange(
-            false,
-            true,
-            Ordering::Acquire,
-            Ordering::Relaxed
-        ).is_ok() {
-            unsafe {
-                let time = libc::time(std::ptr::null_mut());
-                let pid = libc::getpid();
-                let seed_64bit = time ^ (pid as i64);
-                let upper_half = (seed_64bit >> 32) as u32;
-                let lower_half = seed_64bit as u32;
-                let seed = upper_half ^ lower_half;
-                libc::srand(seed);
-            }
-        }
-        unsafe { libc::rand() as u32 }
-    }
-}
-
+static FUNCTION_COUNTER: AtomicU32 = AtomicU32::new(0);
 
 pub(super) fn wasm_declare_init_fn(input: TokenStream) -> ParseResult<TokenStream> {
     if !input.is_empty() {
         return bail!(input, "macro expects no arguments");
     }
 
-    let a = TrivialRng::rand();
-    let b = TrivialRng::rand();
+    let crate_name = env::var("CARGO_PKG_NAME")
+        .expect("CARGO_PKG_NAME env var not found. This macro must be run by Cargo.")
+        .replace('-', "_"); // crate names may contain hyphens, but Rust identifiers must not.
+
+    let index = FUNCTION_COUNTER.fetch_add(1, Ordering::Relaxed);
 
     // Rust presently requires that statics with a custom `#[link_section]` must be a simple
     // list of bytes on the Wasm target (with no extra levels of indirection such as references).
     //
-    // As such, instead we export a function with a random name of known prefix to be used by the embedder.
+    // As such, instead we export a function with a known prefix to be used by the embedder.
     // This prefix is queried at load time, see godot-macros/src/gdextension.rs.
-    let function_name = format_ident!("__godot_rust_registrant_{a}_{b}");
+    let function_name = format_ident!("__godot_rust_registrant_{}_{}", crate_name, index);
 
     let code = quote! {
         #[cfg(target_family = "wasm")] // Strictly speaking not necessary, as this macro is only invoked for Wasm.
